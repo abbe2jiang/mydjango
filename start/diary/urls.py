eecce0b733eb97b86338a4552d0eb9b99ebb4063
Abbe Jiang <abbe@AbbedeMacBook-Pro.local>
2021-01-05 10:12:31 +0400

    init

---
+++ start/diary/urls.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# encoding:utf-8
+'''
+
+Created on Oct 9, 2015
+
+@author: jh
+'''
+from django.conf.urls import url
+
+from diary.views.diaryview import DiaryView
+
+
+urlpatterns = [
+    url(r'^index/$', DiaryView.base,{'action':'index'},name="index"),
+    url(r'^add/$', DiaryView.base,{'action':'add'},name="add"),
+]
